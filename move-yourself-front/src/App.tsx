32aa328f51a5c96c62e78266eb8ea251b7c4c7cd
gusdecante <gustavo.antunes@fatecitapetininga.edu.br>
2021-02-23 12:15:58 -0300

    First day of the next-level-week

---
+++ move-yourself-front/src/App.tsx
@@ -0,0 +1,15 @@
+import GlobalStyle from "./styles/global"
+
+import { ExpecienceBar } from "./components"
+
+
+function App() {
+  return (
+    <div className="container">
+      <GlobalStyle />
+      <ExpecienceBar />
+    </div>
+  );
+}
+
+export default App;
---
+++ move-yourself-front/src/components/index.ts
@@ -0,0 +1,1 @@
+export { default as ExpecienceBar } from "./ExperienceBar"
